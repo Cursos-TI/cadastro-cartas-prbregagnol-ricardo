5cb6fc035b1728e607c292d0c18ec51469c969ee
prbregagnol-ricardo <prbregagnol@gmail.com>
2026-05-28 13:07:49 +0000

    Adicione printf de código!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,8 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+    printf("Olá mundo, no codespace\n");
+    printf("exemplo de atualização de código\n");
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
